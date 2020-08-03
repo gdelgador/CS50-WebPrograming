adabe605e2d2dadc5cf1e40a7b35d4def0a51d54
gdelgador <gonzalo.delgado.r@uni.pe>
2020-08-03 16:03:07 -0500

    New change on js

--- projects3/mail/mail/static/mail/inbox.js
+++ projects3/mail/mail/static/mail/inbox.js
@@ -1,11 +1,10 @@
-
 document.addEventListener('DOMContentLoaded', function() {
-  
   // Use buttons to toggle between views
+  // let mail = null;
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
-  document.querySelector('#compose').addEventListener('click', compose_email(''));
+  document.querySelector('#compose').addEventListener('click', compose_email);
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -19,22 +18,25 @@ function compose_email(mail=null) {
   
   console.log(mail)
   // const
-  const recipients = (mail !== null) ? (mail.recipients.join()) : ('');
+  const recipients = (mail !== null) ? (mail.recipients.join(', ')) : ('');
   const subject = (mail !== null) ? (mail.subject) : ('');
   const body = (mail !== null) ? (mail.body) : ('');
 
   // subject = (subject.substring(0,3).toUpperCase() =='RE:') ? (subject) : ('RE: '+subject);
-  // set values in form in each case
-  document.querySelector('#compose-recipients').value = recipients;
-  document.querySelector('#compose-subject').value = subject;
-  document.querySelector('#compose-body').value = body;
+  // set values in form for each case
+  // document.querySelector('#compose-recipients').value = recipients;
+  // document.querySelector('#compose-subject').value = subject;
+  // document.querySelector('#compose-body').value = body;
 
+  // Clear out composition fields
+  document.querySelector('#compose-recipients').value = '';
+  document.querySelector('#compose-subject').value = '';
+  document.querySelector('#compose-body').value = '';
   // send email
   document.querySelector('#compose-form').onsubmit = send_mail;
 }
 
 function load_mailbox(mailbox) {
-  
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
@@ -43,20 +45,20 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
-
   // adding my changes
   console.log(`${mailbox}`)
   
   fetch(`/emails/${mailbox}`)
   .then(response => response.json())
   .then(emails => {
-    for(var k in emails) {
+    console.log(emails);
+
+    for(var k in emails) {  
       const id = emails[k].id;
       const sender = emails[k].sender;
       const subject = emails[k].subject;
       const timestamp = emails[k].timestamp;
 
-      // 
       // creating new element and adding class
       let element = document.createElement('div');
       element.classList.add('row');
@@ -67,7 +69,7 @@ function load_mailbox(mailbox) {
       element.innerHTML +=`<div class="col-sm">${subject}</div>`;
       element.innerHTML +=`<div class="col-sm">${timestamp}</div>`;
 
-      element.addEventListener('click', () => open_email(id));
+      element.addEventListener('click', () => open_email(id,mailbox));
       // inserting new data 
       document.querySelector('#emails-view').append(element);
       }
@@ -98,11 +100,12 @@ function send_mail() {
       console.log(result);
   }); 
 
+  // reload page - index
   location.reload();
 }
 
-function open_email(mail_id) {
-
+function open_email(mail_id,mailbox) {
+  console.log('opening email: ' + mail_id + 'with type:'+ mailbox);
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'none';
@@ -116,7 +119,7 @@ function open_email(mail_id) {
       console.log(email);
       // change state of email
       update_email_state(email,'read');
-      // let mail = email;
+      let mail = email;
       // const
       const subject = email.subject;
       const from = email.sender;
@@ -133,17 +136,22 @@ function open_email(mail_id) {
       element.innerHTML += `<div><p>${body}</p></div>`;
 
       // buttons functions
-      document.querySelector('#reply').addEventListener('click', () => compose_email(email));
-      
-      document.querySelector('#archive').innerHTML= (email.archived) ? ('Unarchived') : ('Archived');
-      document.querySelector('#archive').addEventListener('click', () => update_email_state(email,'archived'));
+      // document.querySelector('#reply').addEventListener('click', () => compose_email(email));
+      // archive button
+      if ( mailbox !== 'sent') {
+        document.querySelector('#archive').innerHTML= (email.archived) ? ('Unarchived') : ('Archived');
+        document.querySelector('#archive').addEventListener('click', () => update_email_state(email,'archived'));
+      }else{
+        document.querySelector('#archive').style.display = 'none';
+      }
     });
-
-  // document.querySelector('#reply').onclick = compose_email(email);
+    
+  document.querySelector('#reply').onclick = compose_email;
 }
 
 
 function update_email_state(mail,type) {
+  console.log('update email state: '+type);
   if (type == 'archived'){
     const bool_archived = (mail.archived) ? (false) : (true);
 
@@ -157,7 +165,7 @@ function update_email_state(mail,type) {
   }
 
   if (type == 'read') {
-    console.log('process')
+    console.log('read process')
     fetch(`/emails/${mail.id}`, {
       method: 'PUT',
       body: JSON.stringify({read: true})
